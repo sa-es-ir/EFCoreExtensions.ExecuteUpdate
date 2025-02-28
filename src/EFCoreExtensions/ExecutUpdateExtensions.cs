b69b15225c198e0f79f20b0254a6c79d7e37b9c7
sa-es-ir <s.esmaeelinejad@gmail.com>
2025-02-28 22:16:14 +0000

    improve execute update extension

--- src/EFCoreExtensions/ExecutUpdateExtensions.cs
+++ src/EFCoreExtensions/ExecutUpdateExtensions.cs
@@ -13,69 +13,69 @@ public static class ExecutUpdateExtensions
     static MethodInfo UpdateAsyncMethodInfo =
         typeof(EntityFrameworkQueryableExtensions).GetMethod(nameof(EntityFrameworkQueryableExtensions.ExecuteUpdateAsync))!;
 
-    public static int CustomExecuteUpdate(this IQueryable query, string fieldName, object? fieldValue)
+    public static int ExecuteUpdate(this IQueryable query, string fieldName, object? fieldValue)
     {
-        var updateBody = BuildUpdateBody(query.ElementType,
+        var updateBody = SetupUpdateBody(query.ElementType,
             new Dictionary<string, object?> { { fieldName, fieldValue } });
 
         return (int)UpdateMethodInfo.MakeGenericMethod(query.ElementType).Invoke(null, [query, updateBody])!;
     }
 
-    public static Task<int> CustomExecuteUpdateAsync(this IQueryable query, string fieldName, object? fieldValue, CancellationToken cancellationToken = default)
+    public static Task<int> ExecuteUpdateAsync(this IQueryable query, string fieldName, object? fieldValue, CancellationToken cancellationToken = default)
     {
-        var updateBody = BuildUpdateBody(query.ElementType,
+        var updateBody = SetupUpdateBody(query.ElementType,
             new Dictionary<string, object?> { { fieldName, fieldValue } });
 
         return (Task<int>)UpdateAsyncMethodInfo.MakeGenericMethod(query.ElementType).Invoke(null, [query, updateBody, cancellationToken])!;
     }
 
-    public static int CustomExecuteUpdate(this IQueryable query, IReadOnlyDictionary<string, object?> fieldValues)
+    public static int ExecuteUpdate(this IQueryable query, IReadOnlyDictionary<string, object?> fieldValues)
     {
-        var updateBody = BuildUpdateBody(query.ElementType, fieldValues);
+        var updateBody = SetupUpdateBody(query.ElementType, fieldValues);
 
         return (int)UpdateMethodInfo.MakeGenericMethod(query.ElementType).Invoke(null, [query, updateBody])!;
     }
 
-    public static Task<int> CustomExecuteUpdateAsync(this IQueryable query, IReadOnlyDictionary<string, object?> fieldValues, CancellationToken cancellationToken = default)
+    public static Task<int> ExecuteUpdateAsync(this IQueryable query, IReadOnlyDictionary<string, object?> fieldValues, CancellationToken cancellationToken = default)
     {
-        var updateBody = BuildUpdateBody(query.ElementType, fieldValues);
+        var updateBody = SetupUpdateBody(query.ElementType, fieldValues);
 
         return (Task<int>)UpdateAsyncMethodInfo.MakeGenericMethod(query.ElementType).Invoke(null, [query, updateBody, cancellationToken])!;
     }
 
-    static LambdaExpression BuildUpdateBody(Type entityType, IReadOnlyDictionary<string, object?> fieldValues)
+    private static LambdaExpression SetupUpdateBody(Type entityType, IReadOnlyDictionary<string, object?> fieldValues)
     {
-        var setParam = Expression.Parameter(typeof(SetPropertyCalls<>).MakeGenericType(entityType), "s");
-        var objParam = Expression.Parameter(entityType, "e");
+        ArgumentNullException.ThrowIfNull(entityType);
+        ArgumentNullException.ThrowIfNull(fieldValues);
 
-        Expression setBody = setParam;
+        var setParameter = Expression.Parameter(typeof(SetPropertyCalls<>).MakeGenericType(entityType), "s");
+        var entityParameter = Expression.Parameter(entityType, "e");
+
+        Expression setBody = setParameter;
 
         foreach (var pair in fieldValues)
         {
-            var propExpression = Expression.PropertyOrField(objParam, pair.Key);
-            var valueExpression = ValueForType(propExpression.Type, pair.Value);
+            var propertyExpression = Expression.PropertyOrField(entityParameter, pair.Key);
+            var valueExpression = ConvertToExpression(propertyExpression.Type, pair.Value);
 
             setBody = Expression.Call(setBody, nameof(SetPropertyCalls<object>.SetProperty),
-                [propExpression.Type], Expression.Lambda(propExpression, objParam), valueExpression);
+                [propertyExpression.Type], Expression.Lambda(propertyExpression, entityParameter), valueExpression);
         }
 
-        var updateBody = Expression.Lambda(setBody, setParam);
+        var updateBody = Expression.Lambda(setBody, setParameter);
 
         return updateBody;
     }
 
-    static Expression ValueForType(Type desiredType, object? value)
+    private static Expression ConvertToExpression(Type desiredType, object? value)
     {
-        if (value == null)
-        {
-            return Expression.Default(desiredType);
-        }
+        ArgumentNullException.ThrowIfNull(desiredType);
 
-        if (value.GetType() != desiredType)
+        return value switch
         {
-            return Expression.Convert(Expression.Constant(value), desiredType);
-        }
-
-        return Expression.Constant(value);
+            null => Expression.Default(desiredType),
+            _ when value.GetType() != desiredType => Expression.Convert(Expression.Constant(value), desiredType),
+            _ => Expression.Constant(value)
+        };
     }
 }
